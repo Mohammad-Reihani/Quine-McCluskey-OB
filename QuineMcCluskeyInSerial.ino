5e6ca38408eb8b68876996b56d2f7a3bcd1f7466
Mohammad Reihani <mohammad.reihani82@gmail.com>
2024-05-23 03:01:12 +0330

    PrevMessage

--- QuineMcCluskeyInSerial.ino
+++ QuineMcCluskeyInSerial.ino
@@ -1,104 +1,16 @@
 #include <SoftwareSerial.h>
 #include "QuineMcCluskey.h"
 
-// SoftwareSerial pins on ESP8266 NodeMCU (adjust pins as needed)
 SoftwareSerial esp8266Serial(D5, D6);  // RX (GPIO14), TX (GPIO12)
 
-// bool incomingDone = false;
-// const int maxTerms = 100;
-// int minterms[maxTerms];
-// int numMinterms;
-// int dontCares[maxTerms];
-// int numDontCares;
-// bool isMaxTerm;
-
-// QuineMcCluskey qmc;
-
-// void parseReceivedMessage(String& receivedString) {
-//   // // Find the indices of key substrings
-//   // int startIdx = receivedString.indexOf("mainTerms: ") + 11;
-//   // int endIdx = receivedString.indexOf("; numMainTerms:");
-//   // int numMintermsStartIdx = endIdx + 15;
-//   // int numMintermsEndIdx = receivedString.indexOf("; dontCares: ");
-//   // int dontCaresStartIdx = receivedString.indexOf("; ", numMintermsStartIdx);
-
-//   // int lastIntValueStartIdx = numMintermsEndIdx + 2;
-//   // Find the indices of key substrings
-//   int startIdx = receivedString.find("mainTerms: ") + 11;
-//   int endIdx = receivedString.find("; numMainTerms:");
-//   int numMintermsStartIdx = endIdx + 15;
-//   int numMintermsEndIdx = receivedString.find("; dontCares:");
-//   int dontCaresStartIdx = numMintermsEndIdx + 13;
-//   int dontCaresEndIdx = receivedString.find("; numDontCares:", dontCaresStartIdx);
-//   int numDontCaresStartIdx = dontCaresEndIdx + 15;
-//   int numDontCaresEndIdx = receivedString.find("; isMaxTerm:", numDontCaresStartIdx);
-//   int isMaxTermStartIdx = numDontCaresEndIdx + 13;
-
-//   // Check if indices are valid
-//   if (startIdx == -1 || endIdx == -1 || numMintermsStartIdx == -1 || numMintermsEndIdx == -1 || dontCaresStartIdx == -1
-//       || dontCaresEndIdx == -1 || numDontCaresStartIdx == -1 || numDontCaresEndIdx == -1 || isMaxTermStartIdx == -1) {
-//     // Serial.println("Error: Invalid message format");
-//     return;
-//   }
-
-//   // // Extract substrings for minterms, numMinterms, and last integer value
-//   // String mintermsStr = receivedString.substring(startIdx, endIdx);
-//   // String numMintermsStr = receivedString.substring(numMintermsStartIdx, numMintermsEndIdx);
-//   // String lastIntValueStr = receivedString.substring(lastIntValueStartIdx);
-
-//   std::string mintermsStr = receivedString.substr(startIdx, endIdx - startIdx);
-//   std::string numMintermsStr = receivedString.substr(numMintermsStartIdx, numMintermsEndIdx - numMintermsStartIdx);
-//   std::string dontCaresStr = receivedString.substr(dontCaresStartIdx, dontCaresEndIdx - dontCaresStartIdx);
-//   std::string numDontCaresStr = receivedString.substr(numDontCaresStartIdx, numDontCaresEndIdx - numDontCaresStartIdx);
-//   std::string isMaxTermStr = receivedString.substr(isMaxTermStartIdx);
-
-//   // Convert strings to integers
-//   numMinterms = numMintermsStr.toInt();
-//   numDontCares = numDontCaresStr.toInt();
-
-//   if (lastIntValueStr == "0")
-//     isMaxTerm = false;
-//   else
-//     isMaxTerm = true;
-
-  
-
-  
-//   // Extract and store minterms array elements
-//   int commaIdx = mintermsStr.indexOf(',');
-//   int arrayIdx = 0;
-//   while (commaIdx != -1) {
-//     String minterm = mintermsStr.substring(0, commaIdx);
-//     minterms[arrayIdx] = minterm.toInt();
-//     mintermsStr = mintermsStr.substring(commaIdx + 1);  // Skip ","
-//     commaIdx = mintermsStr.indexOf(',');
-//     arrayIdx++;
-//   }
-//   // Last element (no comma after)
-//   minterms[arrayIdx] = mintermsStr.toInt();
-
-
-//   int commaIdx2 = dontCaresStr.indexOf(',');
-//   int arrayIdx2 = 0;
-//   while (commaIdx != -1) {
-//     String dontCares = dontCaresStr.substring(0, commaIdx);
-//     dontCares[arrayIdx] = dontCares.toInt();
-//     dontCaresStr = dontCaresStr.substring(commaIdx + 1);  // Skip ","
-//     commaIdx2 = dontCaresStr.indexOf(',');
-//     arrayIdx2++;
-//   }
-//   // Last element (no comma after)
-//   dontCares[arrayIdx2] = dontCaresStr.toInt();
-// }
-
-
-bool incomingDone = false;
 const int maxTerms = 100;
 int mainTerms[maxTerms];
 int numMainTerms;
 int dontCareTerms[maxTerms];
 int numDontCareTerms;
+
 bool isMaxTerm;
+bool incomingDone = false; //indicates if the data has come and the response has been sent
 
 QuineMcCluskey qmc;
 String response = "";
